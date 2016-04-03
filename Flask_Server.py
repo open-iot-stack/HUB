a5c0a053a8c5a8740359e2bc754493efdbd37102
Shawn <smilligan93@gmail.com>
2016-04-02 22:42:30 -0400

    Implemented printers, commandline arguments, and using requests API

--- Flask_Server.py
+++ Flask_Server.py
@@ -5,9 +5,10 @@ from Gmail_Send import send_email
 from ConfigManager import Config
 import flask
 import sys
+import getopt
 import time
-import urllib2
 import thread
+import requests
 from message_generator import MessageGenerator
 from channel import Channel
 from flask import Flask
@@ -18,6 +19,8 @@ from flask import url_for
 app = Flask(__name__)
 
 SND_PASSWD = ""
+API_KEY = ""
+
 
 nodes = {}
 nlock = thread.allocate_lock()
@@ -29,12 +32,18 @@ conf = Config()
 send_channel, recv_channel = Channel()
 
 def data_collector(uuid, ip, pertype):
+    """Should spawn as its own thread for each sensor
+    that calls activate. Collects data from the sensor every
+    second and dumps it into the send channel.
+    """
+
     global nodes
+    url = "http://" + ip + "/GPIO/2"
     while(True):
         # load the json from the chip
         try:
-            response = urllib2.urlopen("http://" + ip, timeout=3)
-        except urllib2.URLError:
+            response = requests.get(url, timeout=3)
+        except requests.RequestException:
             with nlock:
                 if (ip == nodes[uuid]["ip"]):
                     print "ERROR: Lost Connection to "\
@@ -42,15 +51,30 @@ def data_collector(uuid, ip, pertype):
                     if nodes.has_key(uuid):
                         nodes.pop(uuid)
             thread.exit()
-        str_payload = response.read()
-        json_payload = json.loads(str_payload)
-        if (uuid != json_payload["UUID"]):
-            # TODO: Make a get request on the chip telling it to reconnect
-            pass
-        send_channel.send(json_payload)
+        if response.status_code != requests.codes.ok:
+            print "ERROR: Response from "\
+                    + uuid + " returned status code " + response.status_code
+        else:
+            if r.headers.get('content-type') != 'application/json':
+                print "ERROR: Response from "\
+                        + uuid + " was not in json format"
+            r_json = response.json()
+            if (uuid != r_json["UUID"]):
+                # TODO: Make a get request on the chip telling it to reconnect
+                thread.exit()
+            send_channel.send({
+                uuid: {
+                    pertype : r_json["DATA"]
+                } 
+            })
         time.sleep(1)
 
 def data_receiver():
+    """Should spawn as its own thread. Will take data that
+    is collected by the sensors and send it to the Web API.
+    Also is in charge of logging data based on UUID.
+    """
+
     for message in MessageGenerator(recv_channel):
         print message
     #pass
@@ -60,6 +84,7 @@ def print_action(uuid, action):
     """Post request to do a print action. UUID must match a printer
     type in the config file
     """
+
     if action == "start":
         pass
     elif action == "pause":
@@ -74,8 +99,8 @@ def print_action(uuid, action):
 def sensor_data(uuid):
     return str(uuid)
 
-@app.route('/activate')
-def activate_sensor():
+@app.route('/activate', methods=['GET'])
+def activate_peripheral():
     """API call to register a sensor.
     The UUID and IP should be provided in json format
     as a variable "payload"
@@ -83,6 +108,7 @@ def activate_sensor():
     """
 
     global nodes
+    global printers
     str_payload = request.args.get('payload')
     json_payload = json.loads(str_payload)
     uuid = json_payload["UUID"]
@@ -96,17 +122,19 @@ def activate_sensor():
                         "ip": ip,
                         "type": pertype
                 }
+            return str(printers)
         else:
             with nlock:
                 nodes[uuid] = {
                         "ip": ip,
                         "type": pertype
                 }
-        thread.start_new_thread(data_collector, (uuid, ip, pertype))
-    return str(nodes)
+            thread.start_new_thread(data_collector, (uuid, ip, pertype))
+            return str(nodes)
+    return str(False)
 
-@app.route('/register',methods=['GET', 'POST'])
-def register_sensor():
+@app.route('/register', methods=['GET', 'POST'])
+def register_peripheral():
     """Webpage to register the type of a given sensor
     This should be done before activating the sensor.
     :returns: webpage of result
@@ -129,8 +157,8 @@ def register_sensor():
 def index():
     uuid=0
     return "0 represents the UUID" + '<br>'\
-         + url_for('register_sensor') + '<br>'\
-         + url_for('activate_sensor') + '<br>'\
+         + url_for('register_peripheral') + '<br>'\
+         + url_for('activate_peripheral') + '<br>'\
          + url_for('print_action' ,uuid=uuid, action='start') + '<br>'\
          + url_for('print_action' ,uuid=uuid, action='cancel') + '<br>'\
          + url_for('print_action' ,uuid=uuid, action='pause') + '<br>'\
@@ -146,7 +174,26 @@ def index():
 #              )
     return "Message sent"
 
-if __name__ == '__main__':
-    #SND_PASSWD = sys.argv[1]
+def main(argv):
+    try:
+        opts, args = getopt.getopt(argv, "ha:p:", ["apikey","pass","help"])
+    except getopt.GetoptError, err:
+        # Print debug info
+        print str(err)
+        sys.exit(2)
+    
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            print "Usage: run.py -a <apikey> -p <gmail pass>"
+            sys.exit(0)
+        elif opt in ("-a", "--apikey"):
+            global API_KEY
+            API_KEY = arg
+        elif opt in ("-p", "--pass"):
+            global SND_PASSWD
+            SND_PASSWD = arg
     #app.run(host="192.168.0.1") #need to test this
     app.run(debug=True)
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
