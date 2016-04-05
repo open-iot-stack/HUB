5edb293938ea68a6356247b810c706ed25a56883
Shawn Milligan <smilligan93@gmail.com>
2016-04-05 17:07:40 -0400

    added load_data function to ConfigManager to make sure data is in cache. Couple bug fixes and code cleanup

--- ConfigManager.py
+++ ConfigManager.py
@@ -10,46 +10,40 @@ class Config():
     the configuration. This is to add an API layer for the
     config implementation"""
 
-    _fpath = "config/test.config" # file path of the config file
-    _flock = thread.allocate_lock() # file lock to in case shared across threads
+    _fpath = ".config" # file path of the config file
+    _flock = thread.allocate_lock() # file lock in case shared across threads
     _modified = True
     _cache = {}
 
-    def __init__(self, fpath=""):
+    def __init__(self, fpath=None):
         """TODO: to be defined1. """
-        if fpath != "":
+        if fpath:
             self._fpath = fpath
 
-    def set_config(self, fpath):
+    def set_path(self, fpath):
         """Clears the cache and sets a new file for the config
         :fpath: new filepath for the new Config object
-        :returns: new Config object
+        :returns: boolean on if new config file exists
         """
-        self._fpath = fpath
 
         assert(type(fpath) == str)
-        new_config = Config()
-        new_config._fpath = fpath
-        return new_config
+        self._fpath = fpath
+        self._modified = True
+        self._cache = {}
+        return self.load_data()
 
-    def read_data(self):
-        """Returns a dictionary list of UUIDs and Types
-        Should be used everytime data is accessed for most
-        up to date data.
-
-        :returns: dictionary of uuid and types
+    def load_data(self):
+        """Loads data from file into cache. Happens on read_data as
+        well but can be called earlier to make sure it's loaded in
+        the future. If the config file is not found, return false
+        :returns: boolean on success/failure
         """
         u_types = {}
         with self._flock:
             if not os.path.isfile(self._fpath):
-                return {}
+                return False
             with open(self._fpath, 'r') as f:
-                last_modified = os.path.getmtime(self._fpath)
-                if  self._modified == False:
-                    return self._cache
                 lines = f.readlines()
-                self._modified = False
-                self._cache = u_types
         for line in lines:
             if line.startswith("#"):
                 pass
@@ -62,6 +56,39 @@ class Config():
                     print "Too many spaces in config file, please check", fpath
                     exit()
                 u_types[uuid] = chip_type
+        self._cache = u_types
+        self._modified = False
+        return True
+
+    def read_data(self):
+        """Returns a dictionary list of UUIDs and Types
+        Should be used everytime data is accessed for most
+        up to date data.
+
+        :returns: dictionary of uuid and types
+        """
+        u_types = {}
+        with self._flock:
+            if  self._modified == False:
+                return self._cache
+            if not os.path.isfile(self._fpath):
+                return {}
+            with open(self._fpath, 'r') as f:
+                lines = f.readlines()
+        for line in lines:
+            if line.startswith("#"):
+                pass
+            elif line.startswith("\n"):
+                pass
+            else:
+                try:
+                    uuid, chip_type = line.replace('\n', '').split(" ")
+                except ValueError:
+                    print "Too many spaces in config file, please check", fpath
+                    exit()
+                u_types[uuid] = chip_type
+        self._cache = u_types
+        self._modified = False
         return u_types
 
     def add_data(self, data):
@@ -136,6 +163,7 @@ def dict_shared(dict1, dict2):
 if __name__ == '__main__':
     config = Config()
     #print str(config.read_data())
+    print config.load_data()
     print config.add_data({"21513": "camera"})
     print str(config.read_data())
     print config.add_data({"21141": "camera"})
