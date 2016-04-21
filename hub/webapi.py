847d7060a346c776fc308b246d83a5f07b0116b0
Shawn Milligan <smilligan93@gmail.com>
2016-04-21 15:53:03 -0400

    more work on functions. Starting handling errors within the functions itself for bad status codes

--- hub/webapi.py
+++ hub/webapi.py
@@ -3,11 +3,100 @@
 
 import os
 import hub
+import auth
+
+#DO NOT EDIT HEADERS MANUALY
+headers = {}
+
+def update_headers(web_url):
+    '''Updates the global headers object
+    :web_url: Base webaddress of server, or ip
+    :returns: boolean of success of update
+
+    '''
+    global headers
+    api_key = hub.API_KEY
+    headers = auth.get_headers(api_key, headers=headers,
+                                    base_url=web_url)
+    if headers:
+        return True
+    return False
+
+def add_printer(web_url, printer):
+    '''Add a printer on the web api
+    :web_url: Base webaddress of server, or ip
+    :printer: printer to be added to web api
+    :returns: Tuple of boolean of success and 
+              status code of response. Status code
+              is None if no response
+
+    '''
+    hub_id = hub.ID
+    uuid = printer.get('uuid')
+    url = web_url + '/hubs/' + str(hub_id) + '/printers'
+    try:
+        r = requests.post(url, headers=headers,
+                            json=printer, timeout=3)
+    except requests.ConnectionError:
+        log.log('ERROR: Could not connect to ' + url 
+                + '. Unable to add printer ' + str(uuid) + '.')
+        return False, None
+    except requests.exceptions.Timeout:
+        log.log('ERROR: Timeout when contacting ' + url
+                + '. Unable to add printer ' + str(uuid) + '.')
+        return False, None
+    code = r.status_code
+    if code == 401:
+        log.log('ERROR: Printer ' + str(uuid) + ' not added.'
+                + ' Server responded with ' + str(401) 
+                + ' on ' + url)
+        ret = update_headers(web_url)
+        return add_printer(web_url, printer)
+    if r.status_code != requests.codes.created:
+        log.log('ERROR: Printer ' + str(uuid) + ' not added.'
+                + ' Server responded with ' + str(r.status_code) 
+                + ' on ' + url)
+        return False, r.status_code
+    log.log('Added printer ' + str(uuid) + ' to ' + url )
+    return True, r.status_code
+
+def patch_printer(web_url, headers, printer):
+    '''Patch a printer on the web api
+    :web_url: Base webaddress of server, or ip
+    :headers: headers for request to use
+    :printer: printer to be updated on the web api
+    :returns: Tuple of boolean of success and 
+              status code of response. Status code
+              is None if no response
+
+    '''
+    uuid = printer.get('id')
+    url = web_url + '/printers/' + str(uuid) 
+    try:
+        r = requests.post(url, headers=headers,
+                            json=printer, timeout=3)
+    except requests.ConnectionError:
+        log.log('ERROR: Could not connect to ' + url 
+                + '. Unable to update printer ' + str(uuid) + '.')
+        return False, None
+    except requests.exceptions.Timeout:
+        log.log('ERROR: Timeout when contacting ' + url
+                + '. Unable to update printer ' + str(uuid) + '.')
+        return False, None
+    if r.status_code != requests.codes.created:
+        log.log('ERROR: Printer ' + str(uuid) + ' not updated.'
+                + ' Server responded with ' + r.status_code 
+                + ' on ' + url)
+        return False, r.status_code
+    log.log('Updated printer ' + str(uuid) + ' on ' + url )
+    return True, r.status_code
 
 def add_job(web_url, headers, job):
     '''Will add a job to the WebAPI.
     Handles errors and logs.
-    web_url: Base webaddress of server, or ip
+    :web_url: Base webaddress of server, or ip
+    :headers: headers for request to use
+    :job:     job to be added to web api
     :returns: Tuple of boolean of success and 
               status code of response. Status code
               is None if no response
@@ -38,7 +127,9 @@ def add_job(web_url, headers, job):
 def patch_job(web_url, headers, job):
     '''Will updated a job on the WebAPI.
     Handles errors and logs.
-    web_url: Base webaddress of server, or ip
+    :web_url: Base webaddress of server, or ip
+    :headers: headers for request to use
+    :job:     job to be updated on the web api
     :returns: Tuple of boolean of success and 
               status code of response. Status code
               is None if no response
@@ -70,61 +161,102 @@ def patch_job(web_url, headers, job):
     log.log('Updated job ' + str(job_id) + ' on ' + url)
     return True, r.status_code
 
-def add_printer(web_url, headers, printer):
-    '''Add a printer on the web api
-    web_url: Base webaddress of server, or ip
+def delete_job(web_url, headers, job):
+    '''Will delete a job on the WebAPI
+
+    :web_url: Base webaddress of server, or ip
+    :headers: headers for request to use
+    :job:     job to be deleted to web api
+    :returns: Tuple of boolean of success and 
+              status code of response. Status code
+              is None if no response
+
+    '''
+    printer_id = job.get('printer')
+    job_id = job.get('id')
+    url = web_url + '/jobs/' + str(job_id)
+    try:
+        r = requests.delete(url, headers=headers,
+                            timeout=3)
+    except requests.ConnectionError:
+        log.log('ERROR: Could not connect to ' + url 
+                + '. Unable to delete job' + str(job_id) + '.')
+        return False, None
+    except requests.exceptions.Timeout:
+        log.log('ERROR: Timeout when contacting ' + url
+                + ' Unable to delete job' + str(job_id) + '.')
+        return False, None
+    if r.status_code == requests.codes.unauthorized:
+        log.log('ERROR: Job ' + str(job_id) + ' not updated.'
+                + ' Unauthorized on ' + url)
+        return False, r.status_code
+    elif r.status_code != requests.codes.ok:
+        log.log('ERROR: Job ' + str(job_id) + ' not updated.'
+                + ' Server responded with ' + str(r.status_code)
+                + ' on ' + url)
+        return False, r.status_code
+    log.log('Updated job ' + str(job_id) + ' on ' + url)
+    return True, r.status_code
+
+def add_node(web_url, headers, node):
+    '''Add a node to the web api
+    :web_url: Base webaddress of server, or ip
+    :headers: headers for request to use
+    :node: node to be added to web api
     :returns: Tuple of boolean of success and 
               status code of response. Status code
               is None if no response
 
     '''
     hub_id = hub.ID
-    uuid = printer.get('uuid')
-    url = web_url + '/hubs/' + str(hub_id) + '/printers'
+    node_id = node.get('id')
+    url = web_url + '/hubs/' + str(hub_id) + '/sensors'
     try:
         r = requests.post(url, headers=headers,
-                            json=printer, timeout=3)
+                            json=node, timeout=3)
     except requests.ConnectionError:
         log.log('ERROR: Could not connect to ' + url 
-                + '. Unable to add printer ' + str(uuid) + '.')
+                + '. Unable to add node ' + str(node_id) + '.')
         return False, None
     except requests.exceptions.Timeout:
         log.log('ERROR: Timeout when contacting ' + url
-                + '. Unable to add printer ' + str(uuid) + '.')
+                + '. Unable to add node ' + str(node_id) + '.')
         return False, None
     if r.status_code != requests.codes.created:
-        log.log('ERROR: Printer ' + str(uuid) + ' not added.'
+        log.log('ERROR: Node ' + str(node_id) + ' not added.'
                 + ' Server responded with ' + str(r.status_code) 
                 + ' on ' + url)
         return False, r.status_code
-    log.log('Added printer ' + str(uuid) + ' to ' + url )
+    log.log('Added Node ' + str(node_id) + ' to ' + url )
     return True, r.status_code
 
-def patch_printer(web_url, headers, printer):
-    '''Patch a printer on the web api
-    web_url: Base webaddress of server, or ip
+def patch_node(web_url, headers, node):
+    '''Patch a node on the web api
+    :web_url: Base webaddress of server, or ip
+    :headers: headers for request to use
+    :node: node to be updated on web api
     :returns: Tuple of boolean of success and 
               status code of response. Status code
               is None if no response
 
     '''
-    uuid = printer.get('id')
-    url = web_url + '/printers/' + str(uuid) 
+    node_id = node.get('id')
+    url = web_url + '/sensors/' + str(node_id)
     try:
         r = requests.post(url, headers=headers,
-                            json=printer, timeout=3)
+                            json=node, timeout=3)
     except requests.ConnectionError:
         log.log('ERROR: Could not connect to ' + url 
-                + '. Unable to update printer ' + str(uuid) + '.')
+                + '. Unable to update node ' + str(node_id) + '.')
         return False, None
     except requests.exceptions.Timeout:
         log.log('ERROR: Timeout when contacting ' + url
-                + '. Unable to update printer ' + str(uuid) + '.')
+                + '. Unable to update node ' + str(node_id) + '.')
         return False, None
     if r.status_code != requests.codes.created:
-        log.log('ERROR: Printer ' + str(uuid) + ' not updated.'
-                + ' Server responded with ' + r.status_code 
+        log.log('ERROR: Node ' + str(node_id) + ' not updated.'
+                + ' Server responded with ' + str(r.status_code) 
                 + ' on ' + url)
         return False, r.status_code
-    log.log('Updated printer ' + str(uuid) + ' on ' + url )
+    log.log('Updated Node ' + str(node_id) + ' to ' + url )
     return True, r.status_code
