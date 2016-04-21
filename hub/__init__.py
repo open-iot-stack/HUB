00f6a427c4e3dd0cb69c50e7baf2018b2b2ad3e9
Shawn Milligan <smilligan93@gmail.com>
2016-04-21 13:55:35 -0400

    added an ID for the hub

--- hub/__init__.py
+++ hub/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from hub.config import Config
 from hub.channel import Channel
 from hub.logger import Log
-import hub.auth
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = "uploads/"
@@ -10,6 +9,7 @@ app.config['UPLOAD_FOLDER'] = "uploads/"
 import hub.printers
 import hub.nodes
 import hub.base
+import hub.auth
 
 conf = Config()
 send_channel, recv_channel = Channel()
@@ -19,3 +19,5 @@ log        = None
 SND_PASSWD = None
 API_KEY    = None
 WEB_API    = hub.auth.dev_url
+ID         = 0
+#TODO get correct ID
