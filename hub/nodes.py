86ece89c93fce6a547dd55e0020dd7de5d1f6092
Shawn Milligan <smilligan93@gmail.com>
2016-04-22 19:21:44 -0400

    update to sensors/activate to handle url args. now expects 'id' and 'ip' in just argument format. Also logs if wasn't registered

--- hub/nodes.py
+++ hub/nodes.py
@@ -12,8 +12,8 @@ from dealer import sensor_data_collector
 
 nodes = Chest()
 
-@app.route('/sensors/<int:uuid>/data', methods=['GET'])
-def sensor_data(uuid):
+@app.route('/sensors/<int:id>/data', methods=['GET'])
+def sensor_data(id):
     global nodes
     #uuid = str(uuid)
     return str(nodes.data)
@@ -23,7 +23,7 @@ def activate_sensor(payload = None):
     """API call to activate a sensor on the hub.
     The sensor should provide a parameter 'payload' in
     json format that contains it's IP address as "ip",
-    uuid as "uuid", and port as "port"
+    id as "id", and port as "port"
     :returns: TODO
     """
 
@@ -32,30 +32,30 @@ def activate_sensor(payload = None):
         str_payload = request.args.get('payload')
         payload     = json.loads(str_payload)
 
-    uuid = int(payload.get("uuid"))
-    ip   = payload.get("ip")
-    port = int(payload.get("port", 80))
+    id   = request.args.get("id")
+    ip   = request.args.get("ip")
+    port = int(request.args.get("port", 80))
     conf_data = hub.conf.read_data()
 
     #TODO make dynamic registering by going through different GPIO
     # ports and when you get a response that's the type
-    if uuid in conf_data.keys():
-        pertype = conf_data.get(uuid)
+    if id in conf_data.keys():
+        pertype = conf_data.get(id)
         with nodes.lock:
             # THIS REQUIRES NODES THREADS TO REMOVE THEMSELVES
-            if uuid in nodes.data:
-                return json.jsonify({"message": str(uuid)
+            if id in nodes.data:
+                return json.jsonify({"message": str(id)
                                     + " was already activated"})
-            nodes.data[uuid] = {
-                    "uuid": uuid,
+            nodes.data[id] = {
+                    "id":   id,
                     "ip"  : ip,
                     "type": pertype,
                     "port": port
             }
-        thread.start_new_thread(sensor_data_collector, (uuid, ip, pertype))
-        return json.jsonify({"message": str(uuid) + " has been activated."})
+        thread.start_new_thread(sensor_data_collector, (id, ip, pertype))
+        return json.jsonify({"message": str(id) + " has been activated."})
 
-    #TODO Log the fact that the sensor must be registered.
+    log.log("ERROR: Node " + str(id) + " tried to activate but was never registered")
     abort(400)
 
 @app.route('/sensors/list')
