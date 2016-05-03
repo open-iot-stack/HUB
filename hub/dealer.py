445c5c629b3cd626b63f7be7c8bd5b74855cc3e5
Shawn Milligan <smilligan93@gmail.com>
2016-05-03 13:13:39 -0400

    added NodeCollector object that will be responsible for keeping track of nodes and the sensors it holds

--- hub/dealer.py
+++ hub/dealer.py
@@ -221,6 +221,7 @@ class JobCollector(threading.Thread):
                 cjob.state("paused")
             if status == "printing":
                 cjob.state("printing")
+
             response = octopi.get_job_info(url, key)
             if response != None:
                 failures = 0
@@ -229,11 +230,11 @@ class JobCollector(threading.Thread):
                 log.log("ERROR: Could not collect"
                        + " job data from printer " + str(id)
                        + " on " + url )
-                continue
                 if failures > 5:
                     cjob.state("errored")
                     webapi.patch_job(cjob.to_web(recent_json))
                     return -1
+                continue
             if response.status_code != 200:
                 log.log("ERROR: Response from "
                         + str(id) + " returned status code "
@@ -258,6 +259,79 @@ class JobCollector(threading.Thread):
     def stop(self):
         self.stopped = True
 
+class NodeCollector(threading.Thread):
+
+    """NodeCollector will collect sensor information
+    from the sensors associated with it"""
+
+    def __init__(self, node_id, webapi):
+        """TODO: to be defined1.
+
+        """
+        threading.Thread.__init__(self)
+        self.node_id = node_id
+        self.webapi = webapi
+        self.stopped = False
+
+    def stop(self):
+        self.stopped = True
+
+    def run(self):
+        """TODO: Docstring for run.
+        :returns: TODO
+
+        """
+        id     = self.node_id
+        webapi = self.webapi
+        node   = Node.get_by_id(id)
+        log.log("NodeCollector starting for node " + str(id))
+        failures = 0
+        while(True):
+            node    = Node.get_by_id(id, fresh=True)
+            ip      = node.ip
+            sensors = node.sensors
+            if self.stopped == True:
+                log.log("NodeCollector for node " + str(id)
+                        + " was requested to stop. Exiting...")
+                return 0
+            for sensor in sensors:
+                pin         = sensor.pin
+                webid       = sensor.webid
+                sensor_type = sensor.sensor_type
+                if pin == None or webid == None or sensor_type == None:
+                    continue
+                url = "http://" + ip + "/gpio/"
+                            + str(pin) + "/" + str(sensor_type)
+                try:
+                    response = requests.get(url, timeout=10)
+                except requests.ConnectionError:
+                    log.log("ERROR: Could not connect to " + url)
+                    response = None
+                except requests.exceptions.Timeout:
+                    log.log("ERROR: Timeout occured on " + url)
+                    response = None
+                if response == None:
+                    failures += 1
+                    if failures > 10:
+                        log.log("ERROR: Could not collect"
+                                + " sensor data from node " + str(id)
+                                + " on url " + url)
+                        return -1
+                    continue
+                if response.status_code != 200:
+                    log.log("ERROR Response from " 
+                            + str(id) + " returned status code "
+                            + str(response.status_code) + " on "
+                            + response.url)
+                else:
+                    recent_json = response.json()
+                    data = sensor.to_web(recent_json)
+                    if data != None:
+                        webapi.add_data(data)
+                sleep(1)
+            sleep(1)
+
+
 def get_temp(node_ip, gpio):
     """Creates request to dht node for data"""
 
@@ -278,76 +352,3 @@ def get_gpio(node_ip, gpio, type):
     if data['data']:
         return data["data"]
     return "{'data':error}"
-
-
-def node_data_collector(id, ip):
-    """Should spawn as its own thread for each node
-    that calls activate. Collects data from the node every
-    second and dumps it into the send channel.
-
-    """
-    nodes        = hub.nodes.nodes
-    log          = hub.log
-    failures     = 0
-    #print str(hub.print_enabled)
-    #TODO a lot of this code is fulling working/tested but general idea is there
-    while(True):
-        # load the json from the chip
-        if failures > 20:
-            log.log("ERROR: Have failed communication 20 times in a row."
-                  + " Closing connection with " + str(id))
-            with nodes.lock:
-                if nodes.data.has_key(id):
-                    nodes.data.pop(id)
-            return
-        try:
-            #TODO: get all sensors attached to node then call the correct
-            #method to get data
-            results = Sensor.query.filter_by(node_id=nid).all()
-            json_results = []
-
-            for result in results:
-                if result.sensor_type == 'temp':
-                    temp, humidity = get_temp(ip, result.pin)
-                else:
-                    data = get_gpio(ip, result.pin, result.sensor_type)
-            failures = 0
-        except requests.Timeout:
-            failures += 1
-            log.log("ERROR: Timeout occured when communicating with "
-                    + str(id) + ".")
-            continue
-        except requests.ConnectionError:
-            failures += 1
-            with nodes.lock:
-                if (ip == nodes.data[id]["ip"]):
-                    log.log("ERROR: Lost Connection to "
-                            + str(id) + ".")#" Thread exiting...")
-            continue
-    # Start handling of data here.
-    """
-        if response.status_code != requests.codes.ok:
-            log.log("ERROR: Response from "
-                    + str(id) + " returned status code "
-                    + response.status_code)
-
-        else:
-            r_json = response.json()
-            #TODO: Could break current implementation
-            #send_channel.send({
-            #    id: {
-            #        pertype : r_json.get("data")
-            #    }
-            #})
-    """
-    time.sleep(1)
-
-def get_extension(name):
-    """Returns the extension of a file.
-    :path: path of file to get the extension of
-    :returns: file extension
-
-    """
-    ext = name.rsplit('.', 1)[1]
-    return ext
-    
