bdb39ea26424a11c0a9d0e35bab6fb1900770bca
Shawn Milligan <smilligan93@gmail.com>
2016-05-05 18:19:57 -0400

    fixed location of systemd install to work on pi

--- install.py
+++ install.py
@@ -19,7 +19,7 @@ def systemd_setup(config):
             line = line.replace("<DIRECTORY>",cwd)
             line = line.replace("<COMMAND>", command)
             new.append(line)
-    with open("/usr/lib/systemd/system/stratusprint-hub.service", "w+") as f:
+    with open("/lib/systemd/system/stratusprint-hub.service", "w+") as f:
         f.writelines(new)
     with open("/dev/null","w") as out:
         subprocess.call(['systemctl','daemon-reload'],stdout=out)
